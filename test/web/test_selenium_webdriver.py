5a3d7dccd7c6a44a0483f02af16fe0842b3dfaf6
jehrodrigues1101 <rodrigues.jessica1101@gmail.com>
2021-12-13 18:31:11 -0300

    testes web

--- test/web/test_selenium_webdriver.py
+++ test/web/test_selenium_webdriver.py
@@ -1,12 +1,24 @@
 # 1 importar bibliotecas
+import os
+from datetime import datetime
+
 import pytest
 # Classes
 from selenium import webdriver
 from selenium.webdriver.common import keys
 from selenium.webdriver.common.by import By
 
+caminho_print = 'C:/Users/jessi/PycharmProjects/fts132_inicial3/prints/' \
+                + datetime.now().strftime('%Y-%m-%d %H-%M-%S') + '/'
+
 
 class Test_selenium_webdriver:
+
+    def before_all(self):
+
+    # Criar pasta para guardar prints
+        os.mkdir(caminho_print)
+
     # Definição de inicio- Executa antes do teste
     def setup_method(self):
         # Declarar o Objeto do Selenium e instanciar como navegador desejado
@@ -22,18 +34,20 @@ class Test_selenium_webdriver:
         self.driver.quit()
 
     # Definição do teste
-    @pytest.mark.parametrize('termo, curso, preco',[
-        ('mantis', 'Mantis', 'R$ 59,99'),
-        ('ctfl', 'Preparatório CTFL', 'R$ 199,00'),
+    @pytest.mark.parametrize('id, termo, curso, preco', [
+        ('1', 'mantis', 'Mantis', 'R$ 59,99'),
+        ('2', 'ctfl', 'Preparatório CTFL', 'R$ 199,00'),
     ])
-    def testar_comprar_curso_mantis_com_click_na_lupa(self, termo, curso, preco):
+    def testar_comprar_curso_click_na_lupa(self, id, termo, curso, preco):
         self.driver.get('https://www.iterasys.com.br')  # Selenium abra a url indicado
+        self.driver.get_screenshot_as_file(f'{caminho_print}teste {id} - passo 1 - home.png')
         # Selenium clica no elemento
         self.driver.find_element(By.ID, 'searchtext').click()
         # O selenium apaga o conteudo da caixa de pesquisa
         self.driver.find_element(By.ID, 'searchtext').clear()
         # O selenium escreve mantis na caixa de pesquisa
         self.driver.find_element(By.ID, 'searchtext').send_keys(termo)
+        self.driver.get_screenshot_as_file(f'{caminho_print}teste {id} - passo 2 - pesquisa pelo nome.png')
         # selenium clica no botão da lupa
         self.driver.find_element(By.ID, 'btn_form_search').click()
         # Selenium cliaca em matricule-se
@@ -57,5 +71,3 @@ class Test_selenium_webdriver:
         # O selenium valida o nome do curso no carrinho de compras
         assert self.driver.find_element(By.CSS_SELECTOR, 'span.item-title').text == 'Mantis'
         assert self.driver.find_element(By.CSS_SELECTOR, 'span.new-price').text == 'R$ 59,99'
-
-
